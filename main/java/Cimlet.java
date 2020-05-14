82ba804b7e0b60401d26c1ef6be7be850dfde25f
vargaendre84 <50341903+vargaendre84@users.noreply.github.com>
2020-05-14 14:35:23 +0200

    Delete Cimlet.java

--- main/java/Cimlet.java
+++
@@ -1,8 +0,0 @@
-public class Cimlet
-{
-    private static int cimlet = 100000;
-    private static int cimletekSzama = 1000000;
-
-    public static int getCimletErtek() {return cimlet;}
-    public static int getCimletekMaxSzama() {return cimletekSzama;}
-}
